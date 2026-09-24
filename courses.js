6948c3cc8550d123b10415f63bda4291e5812161
Robert <robert.michels1233@gmail.com>
2026-09-24 11:12:49 +0200

    feat: add Verbier Les Esserts

--- courses.js
+++ courses.js
@@ -86,7 +86,6 @@ const COURSES = {
   },
   'Grandvalira Golf Soldeu': {
     // https://www.grandvalira.com/en/golf-soldeu
-    // Yellow tee
     par: 33, ratingPar: 66,
     defaultTee: 'Yellow',
     tees: [
@@ -106,8 +105,39 @@ const COURSES = {
       { par: 5, si: 1  },
       { par: 3, si: 9 },
     ]
+  },
+  'Verbier Les Esserts': {
+    // https://www.verbiergolfclub.ch/les-esserts/
+    par: 69, ratingPar: 69,
+    defaultTee: 'Yellow',
+    tees: [
+      { colour: 'White',  sss: 67.4, slope: 133 },
+      { colour: 'Yellow', sss: 66.6, slope: 131 },
+      { colour: 'Blue',   sss: 64.2, slope: 128 },
+      { colour: 'Red',    sss: 64.0, slope: 128 },
+    ],
+    holes: [
+      { par: 3, si: 10 },
+      { par: 5, si: 6 },
+      { par: 4, si: 2 },
+      { par: 3, si: 16 },
+      { par: 5, si: 14 },
+      { par: 5, si: 4 },
+      { par: 3, si: 18 },
+      { par: 4, si: 12 },
+      { par: 3, si: 8 },
+      { par: 4, si: 17 },
+      { par: 4, si: 1 },
+      { par: 3, si: 15 },
+      { par: 4, si: 3 },
+      { par: 4, si: 9 },
+      { par: 4, si: 13 },
+      { par: 4, si: 5 },
+      { par: 4, si: 7 },
+      { par: 3, si: 11 },
+    ]
   }
 };
 
 // Names shown as buttons in the lobby (last entry should always be 'Others')
-const PRESET_COURSES = ['Golfclub St Genis', 'Ugolf Aravella Andorra', 'Grandvalira Golf Soldeu', 'Others'];
+const PRESET_COURSES = ['Golfclub St Genis', 'Verbier Les Esserts', 'Ugolf Aravella Andorra', 'Grandvalira Golf Soldeu', 'Others'];
